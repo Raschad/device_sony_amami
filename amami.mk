73ca89743f86c5dc09e9b77be4081178f22babf0
Raschad <Raschad@yandex.ru>
2019-01-14 22:22:54 +0300

    Update amami.mk

--- amami.mk
+++ amami.mk
@@ -49,8 +49,9 @@ PRODUCT_COPY_FILES += \
 
 TARGET_SYSTEM_PROP += device/sony/amami/system.prop
 
-# setup dalvik vm configs
-$(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
+# setup dalvik vm configs and hwui
+$(call inherit-product, device/sony/amami/phone-xhdpi-2048-dalvik-heap.mk)
+$(call inherit-product, device/sony/amami/phone-xxhdpi-2048-hwui-memory.mk)
 
 # Include non-opensource parts
 $(call inherit-product, vendor/sony/amami/amami-vendor.mk)
