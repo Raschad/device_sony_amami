dd29e5368a1508cb044d4dae19c31e9b967ff52b
Raschad <Raschad@yandex.ru>
2019-01-29 15:43:10 +0300

    Update aosp_amami.mk

--- aosp_amami.mk
+++ aosp_amami.mk
@@ -1,5 +1,5 @@
 # Inherit AICP common Phone stuff.
-$(call inherit-product, vendor/aosp/common.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 $(call inherit-product, device/sony/amami/full_amami.mk)
 
@@ -8,11 +8,11 @@ PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="D5503-user 5.1.1 14.6.A.1.23
 
 BUILD_FINGERPRINT := Sony/D5503/D5503:5.1.1/14.6.A.1.236/2031203603:user/release-keys
 
-# TARGET_GAPPS_ARCH := arm
-# TARGET_DENSITY := xhdpi
+TARGET_GAPPS_ARCH := arm
+TARGET_DENSITY := xhdpi
 TARGET_BOOT_ANIMATION_RES := 720
 
-# PRODUCT_GMS_CLIENTID_BASE := android-sony
+PRODUCT_GMS_CLIENTID_BASE := android-sony
 
 PRODUCT_NAME := aosp_amami
 PRODUCT_DEVICE := amami
@@ -22,5 +22,5 @@ PRODUCT_DEVICE := amami
 #         DEVICE_MAINTAINERS="SpiritCroc"
 
 # Boot animation
-TARGET_SCREEN_HEIGHT := 1280
-TARGET_SCREEN_WIDTH := 720
+# TARGET_SCREEN_HEIGHT := 1280
+# TARGET_SCREEN_WIDTH := 720
